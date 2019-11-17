5832d0ba74a98f35f301590fcccd9727a5178929
mohovkm <mohovkm@ya.ru>
2019-11-17 12:31:15 +0500

    Second project for Yandex

---
+++ install_requirements.sh
@@ -0,0 +1,12 @@
+#activate virtual environment
+python3 -m venv env
+source env/bin/activate
+
+#install packages
+pip3 install pandas
+pip3 install pymystem3
+pip3 install collections
+pip3 install jupyterlab
+
+#run notebook
+jupyter notebook;
